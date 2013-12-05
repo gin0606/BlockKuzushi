c52708f847a29e2e59e92aa286a7f3610241c63c
gin0606 <kkgn06@gmail.com>
2013-12-05 14:39:11 +0900

    add bord

---
+++ Assets/Scripts/Bord.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bord : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.LeftArrow)){
+			 this.transform.position += new Vector3(-0.1f, 0, 0);
+		}else if(Input.GetKey(KeyCode.RightArrow)){
+			 this.transform.position += new Vector3(0.1f, 0, 0);
+		}
+	}
+}
