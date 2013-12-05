e033947fce812513ac22cc43467134273293d117
gin0606 <kkgn06@gmail.com>
2013-12-05 14:13:38 +0900

    add Force to ball on start

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,7 +5,7 @@ public class Ball : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		this.rigidbody2D.AddForce(new Vector2(200, 200));
 	}
 	
 	// Update is called once per frame
