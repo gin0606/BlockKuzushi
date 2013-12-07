84ce9d7133ed6eb2633fa6df921c006380a73451
gin0606 <kkgn06@gmail.com>
2013-12-07 14:46:50 +0900

    add target

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Target : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision){
+		Destroy(this.gameObject);
+	}
+}
